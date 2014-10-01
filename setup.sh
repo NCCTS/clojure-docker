3863009839386970d5ec4991ac36d30b20c75ed3
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-10-01 17:46:24 -0500

    prefer openjdk (distribution legality considerations)

--- setup.sh
+++ setup.sh
@@ -5,17 +5,21 @@ export HOME=/root
 
 # Install packages
 apt-get update
-apt-get -y install software-properties-common
-add-apt-repository -y ppa:webupd8team/java
-apt-get update
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt-get install -y oracle-java7-installer
+apt-get -y install openjdk-7-jdk
+
+# Oracle alternative
+# ------------------
+# apt-get -y install software-properties-common
+# add-apt-repository -y ppa:webupd8team/java
+# apt-get update
+# echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+# apt-get -y install oracle-java7-installer
+# apt-get -y remove --auto-remove software-properties-common
 
 # Setup Leiningen / Clojure environment for unprivileged user 'sailor'
 sudo -i -u sailor /docker-build/support/user_sailor.sh
 
 # Cleanup
-apt-get -y remove --auto-remove software-properties-common
 apt-get -y autoclean
 apt-get -y clean
 apt-get -y autoremove
