27f921a57169683095d3ae2efe1b1b7e776b30b3
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-01-23 03:22:49 -0600

    bump deps

--- support/user_sailor.sh
+++ support/user_sailor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir bin
-wget -O bin/lein https://raw.githubusercontent.com/technomancy/leiningen/2.5.0/bin/lein
+wget -O bin/lein https://raw.githubusercontent.com/technomancy/leiningen/2.5.1/bin/lein
 chmod +x bin/lein
 bin/lein version
 echo "(exit)" | bin/lein repl
